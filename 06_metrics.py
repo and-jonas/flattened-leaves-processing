a388b1a28d295d6cf3e1f1996bed4e026c46aa5b
Jonas Anderegg <jonas.anderegg@agroscope.admin.ch>
2026-08-20 18:28:28 +0200

    adjusted path to scratch

--- 06_metrics.py
+++ 06_metrics.py
@@ -236,8 +236,8 @@ def main(aligned=False):
 
 if __name__ == "__main__":
 
-    base_path = Path("O:/Data-Work/22_Plant_Production-CH/224_Digitalisation/Jonas_Anderegg_Files/E_Work/03_PreDiMix/Uitikon/20260623_Uitikon_Leaf")
-    # base_path = Path(os.environ["SCRATCH"])
+    # base_path = Path("O:/Data-Work/22_Plant_Production-CH/224_Digitalisation/Jonas_Anderegg_Files/E_Work/03_PreDiMix/Uitikon/20260623_Uitikon_Leaf")
+    base_path = Path(os.environ["SCRATCH"])
 
     aligned = False  # set to False if using individual images instead of aligned images
 
